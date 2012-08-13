377604392fa0f162fc9d7926dbb27292730e11a7
Pete Kocks <pk@kocks.com>
2012-08-13 06:48:10 +0000

    removed vim.org downloads

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 echo "installing apt-get packages"
 sudo bash -c 'grep -v "^#" $HOME/dot-files/package_list.ubuntu | xargs -t -I {} sudo apt-get -y install {}' vim/bundle/
 
-echo "configuring vim"
-$HOME/dot-files/vim/update_bundles.rb
+echo "updating vim plugins"
+git submodule init
+git submodule update
 
 echo "linking dots"
 $HOME/dot-files/links.sh
